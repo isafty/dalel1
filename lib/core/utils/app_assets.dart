e87ce965cdc13bba01461cbe0d448f9940bf4c86
isafty <ealsafty@gmail.com>
2024-07-28 14:05:01 +0300

    App Colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesPassword
+  /// assets/images/password.png
+  static const String imagesPassword = "assets/images/password.png";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+
+  /// Assets for imagesSplash2
+  /// assets/images/splash2.png
+  static const String imagesSplash2 = "assets/images/splash2.png";
+
+  /// Assets for imagesSplash3
+  /// assets/images/splash3.png
+  static const String imagesSplash3 = "assets/images/splash3.png";
+
+  /// Assets for imagesSucccesorder
+  /// assets/images/Succcesorder.png
+  static const String imagesSucccesorder = "assets/images/Succcesorder.png";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0XffC49E85);
+  static Color backgroundColor = const Color(0XffF8F4F9);
+  static Color deebBrown = const Color(0Xff6B4B3E);
+  static Color deebGrey = const Color(0Xff6F6460);
+  static Color grey = const Color(0Xff6F6460);
+  static Color lightGrey = const Color(0XffB4B4B4);
+  static Color black = const Color(0Xff333333);
+}
